e2570082730dad2daa8dc0b060465fc5dcfa7fca
Baimanchick <baikaimanchick@gmail.com>
2025-04-18 14:15:31 +0600

    [FIX] fixed default language

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
   locales: ['ru', 'kg', 'en'],
-
   defaultLocale: 'ru',
+  localeDetection: false,
 })
 
 export const config = {
